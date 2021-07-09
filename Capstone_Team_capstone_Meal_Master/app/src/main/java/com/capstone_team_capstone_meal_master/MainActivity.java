1369254de9d4ec12d92643a80c3e02f860a253df
simranpreet92 <simisaggu12@gmail.com>
2021-07-09 02:03:50 -0700

    login UI created

---
+++ Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/MainActivity.java
@@ -0,0 +1,18 @@
+package com.capstone_team_capstone_meal_master;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
