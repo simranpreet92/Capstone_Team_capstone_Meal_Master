a2d735c058eae18c98d68e413691c16cd098518c
jspreetkaur <jassaulakh.00@gmail.com>
2021-07-20 10:42:50 -0700

    cart & discount working

--- Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/CartFragment.java
+++ Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/CartFragment.java
@@ -64,13 +64,21 @@ public class CartFragment extends Fragment {
     private final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
     TextView tvItemTotal, tvGrandTotal, tvDiscountPoints, tvDiscount, tvNoItems;
     Button btApplyPoints, btRemovePoints, btCheckout;
-   // LinearLayout llDiscount, llContent, llOffers;
+    LinearLayout llDiscount, llContent, llOffers;
     long points = 0;
     boolean isPointsApplied = false;
     RelativeLayout rlProgress;
-  //  ProgressBar pBar;
-    //  PayPalConfiguration config;
-    // onOrderPlaced listener;
+    ProgressBar pBar;
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+            Toast.makeText(getContext(), "Working", Toast.LENGTH_SHORT).show();
+        }
+    });
+
+    public CartFragment() {
+
+    }
 
     public static CartFragment newInstance() {
         return new CartFragment();
@@ -97,6 +105,58 @@ public class CartFragment extends Fragment {
         });
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_cart, container, false);
+        tvGrandTotal = view.findViewById(R.id.tvGrandTotal);
+        tvItemTotal = view.findViewById(R.id.tvTotal);
+        tvDiscountPoints = view.findViewById(R.id.tvDiscountPoints);
+        tvDiscount = view.findViewById(R.id.tvDiscount);
+        btApplyPoints = view.findViewById(R.id.btDiscountApply);
+        btRemovePoints = view.findViewById(R.id.btDiscountRemove);
+        llDiscount = view.findViewById(R.id.llDiscount);
+        btCheckout = view.findViewById(R.id.btnCheckout);
+        tvNoItems = view.findViewById(R.id.tvNoItems);
+        rlProgress = view.findViewById(R.id.rlProgress);
+        llContent = view.findViewById(R.id.llContent);
+        pBar = view.findViewById(R.id.pBar);
+        llOffers = view.findViewById(R.id.llOffers);
+        btCheckout.setOnClickListener(v -> {
+            long point = isPointsApplied ? points : 0;
+
+        });
+        btApplyPoints.setOnClickListener(v -> {
+            isPointsApplied = true;
+            btApplyPoints.setVisibility(View.GONE);
+            btRemovePoints.setVisibility(View.VISIBLE);
+            updateLabels();
+        });
+        btRemovePoints.setOnClickListener(v -> {
+            isPointsApplied = false;
+            btRemovePoints.setVisibility(View.GONE);
+            btApplyPoints.setVisibility(View.VISIBLE);
+            updateLabels();
+        });
+        RecyclerView rvCart = view.findViewById(R.id.rvCart);
+        rvCart.setNestedScrollingEnabled(true);
+        rvCart.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvCart.setAdapter(foodAdapter);
+
+        fetchCartData();
+        return view;
+    }
+
+    private void toggleDiscount(boolean flag) {
+        if (flag) {
+            btRemovePoints.setVisibility(View.GONE);
+            btApplyPoints.setVisibility(View.GONE);
+            tvDiscountPoints.setText(R.string.no_points);
+        } else {
+            btApplyPoints.setVisibility(View.VISIBLE);
+        }
+    }
 
     private void updateLabels() {
         if (isPointsApplied) {
@@ -107,4 +167,79 @@ public class CartFragment extends Fragment {
             tvGrandTotal.setText(String.format(Locale.US, "%s %.1f", getContext().getString(R.string.dollar), grandTotal));
         }
     }
+
+    public void fetchCartData() {
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            if (!currentUser.isAnonymous()) {
+                firebaseFirestore.collection("discount").document(uid).get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null && task.getResult().contains("points")) {
+                        points = Math.round(Double.parseDouble(String.valueOf(task.getResult().get("points"))));
+                        tvDiscountPoints.setText(String.format(getString(R.string.available_points), points));
+                        toggleDiscount(points == 0);
+                    } else {
+                        toggleDiscount(false);
+                    }
+                });
+            } else {
+                llDiscount.setVisibility(View.GONE);
+                llOffers.setVisibility(View.GONE);
+            }
+            firebaseFirestore.collection("cart").document(uid).addSnapshotListener((value, error) -> {
+                if (value != null && value.exists()) {
+                    Map<String, Object> data = value.getData();
+                    if (data != null && data.size() > 0) {
+                        firebaseFirestore
+                                .collection("food")
+                                .whereIn("id", Arrays.asList(data.keySet().toArray()))
+                                .get()
+                                .addOnCompleteListener(task -> {
+                                    pBar.setVisibility(View.GONE);
+                                    foodItems.clear();
+                                    total = grandTotal = 0;
+                                    Map<Food, Integer> map = new HashMap<>();
+                                    if (task.isSuccessful() && task.getResult() != null) {
+                                        for (DocumentSnapshot ds : task.getResult().getDocuments()) {
+                                            Food food = ds.toObject(Food.class);
+                                            if (food != null) {
+                                                int quantity = Integer.parseInt(String.valueOf(data.get(food.getId())));
+                                                if (quantity > 0) {
+                                                    foodItems.add(food);
+                                                    map.put(food, quantity);
+                                                    total += food.getPrice() * quantity;
+                                                    grandTotal = total;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    cart.setCartItems(map);
+                                    foodAdapter.notifyDataSetChanged();
+                                    tvItemTotal.setText(String.format(Locale.US, "%s %.1f", getContext().getString(R.string.dollar), total));
+                                    updateLabels();
+                                    if (foodItems.size() > 0) {
+                                        rlProgress.setVisibility(View.GONE);
+                                        llContent.setVisibility(View.VISIBLE);
+                                    } else {
+                                        showEmptyCart();
+                                    }
+                                });
+                    } else {
+                        showEmptyCart();
+                    }
+                } else {
+                    showEmptyCart();
+                }
+            });
+        }
+
+    }
+
+    public void showEmptyCart() {
+        pBar.setVisibility(View.GONE);
+        rlProgress.setVisibility(View.VISIBLE);
+        llContent.setVisibility(View.GONE);
+        tvNoItems.setVisibility(View.VISIBLE);
+    }
+
+
 }
