8927cb1dd14d41e95c1ceb97b8a99aca0bb1cf1d
harpreet kaur <happybasra109@gmail.com>
2021-07-22 12:27:50 -0400

    minor changes

--- Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/OrderFragment.java
+++ Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/OrderFragment.java
@@ -3,14 +3,35 @@ package com.capstone_team_capstone_meal_master;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.capstone_team_capstone_meal_master.adapter.OrderAdapter;
+import com.capstone_team_capstone_meal_master.model.Order;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderFragment extends Fragment {
 
-
+    ProgressBar pBar;
+    RecyclerView rvOrder;
+    TextView tvNoOrder;
+    List<Order> orders = new ArrayList<>();
+    private final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+    OrderAdapter orderAdapter;
 
     public OrderFragment() {
         // Required empty public constructor
@@ -29,8 +50,16 @@ public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_order, container, false);
+        pBar = view.findViewById(R.id.pBar);
+        rvOrder = view.findViewById(R.id.rvOrder);
+        tvNoOrder = view.findViewById(R.id.tvNoOrder);
+        orderAdapter = new OrderAdapter(getContext(), orders);
+        rvOrder.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvOrder.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        rvOrder.setAdapter(orderAdapter);
+        return view;
     }
     private void fetchOrders() {
 
