5ec86d74be35951e16b0e02e0854caa61dbe210e
jspreetkaur <jassaulakh.00@gmail.com>
2021-07-26 00:15:34 -0700

    Facebook login error resolved

--- Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/LoginActivity.java
+++ Capstone_Team_capstone_Meal_Master/app/src/main/java/com/capstone_team_capstone_meal_master/LoginActivity.java
@@ -158,7 +158,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     @Override
                     public void onError(FacebookException exception) {
-                    
+
                     }
                 });
 
